c0383629a67fdeb3a296c0be9cf1e7588d7e4d55
DustinChaloupka <dustin.chaloupka@gmail.com>
2023-04-16 17:49:53 -0500

    Fix disappearing enemies in battle

--- goldensun/memory/game/battlegroup.lua
+++ goldensun/memory/game/battlegroup.lua
@@ -4,7 +4,7 @@ local Chunk = require("goldensun.memory.chunk")
 local BattleGroup = Chunk.new()
 
 function BattleGroup:draw_battle()
-    for i, enemy in ipairs(self:get_enemies()) do enemy:draw_battle(i) end
+    for i, enemy in pairs(self:get_enemies()) do enemy:draw_battle(i) end
 end
 
 function BattleGroup:get_enemies()
@@ -13,13 +13,12 @@ function BattleGroup:get_enemies()
         local enemy = self:new_enemy(i)
         if enemy:get_current_hp() > 0 then enemies[i] = enemy end
     end
-
     return enemies
 end
 
 function BattleGroup:total_enemy_level()
     local level = 0
-    for _, enemy in ipairs(self:get_enemies()) do
+    for _, enemy in pairs(self:get_enemies()) do
         level = level + enemy:get_level()
     end
 
